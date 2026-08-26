f22ef5ff56e4008171dc4afbb0f10d02e25afbc9
Juan Diosdado <juan.diosdado@gmail.com>
2026-08-26 22:24:45 +0200

    Initial Commit

---
+++ KyfkyfJournalDemo/KyfkyfJournalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KyfkyfJournalDemoApp.swift
+//  KyfkyfJournalDemo
+//
+//  Created by Juan Diosdado on 2026-08-26.
+//
+
+import SwiftUI
+
+@main
+struct KyfkyfJournalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
