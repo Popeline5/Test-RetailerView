845e51aed88812e508a886b31b4aae4624694c90
Pauline de Mortain <pauline.de.mortain1@gmail.com>
2026-09-10 08:47:34 +0200

    Started RetailerView - Header - Location details - Wished items - Tried items

---
+++ KyfkyfJournalDemo/Views/Retailer/RetailerView.swift
@@ -0,0 +1,18 @@
+//
+//  RetailerView.swift
+//  KyfkyfJournalDemo
+//
+//  Created by Pauline de Mortain on 09/09/2026.
+//
+
+import SwiftUI
+
+struct RetailerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RetailerView()
+}
