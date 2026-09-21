9f88b371a3a3984cfc933a40ba0511f91519c7d9
Pauline de Mortain <pauline.de.mortain1@gmail.com>
2026-09-21 19:28:25 +0200

    - Tried 3 different versions of location details in RetailerView     - Normal     - ScrollView     - ScrollView + white frame

---
+++ KyfkyfJournalDemo/Views/Retailer/RetailerView_Catalog.swift
@@ -0,0 +1,18 @@
+//
+//  RetailerView_Catalog.swift
+//  KyfkyfJournalDemo
+//
+//  Created by Pauline de Mortain on 21/09/2026.
+//
+
+import SwiftUI
+
+struct RetailerView_Catalog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RetailerView_Catalog()
+}
---
+++ KyfkyfJournalDemo/Views/Retailer/RetailerView_Items.swift
@@ -0,0 +1,18 @@
+//
+//  RetailerView_Items.swift
+//  KyfkyfJournalDemo
+//
+//  Created by Pauline de Mortain on 21/09/2026.
+//
+
+import SwiftUI
+
+struct RetailerView_Items: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RetailerView_Items()
+}
